82b8c9f0d067f787f0cba7b28d080dfa409be586
WYMoe <waiyan.mdycode@gmail.com>
2023-04-18 19:15:07 +0630

    Initial Commit

---
+++ TheMovieSwiftUIPADC/TheMovieSwiftUIPADCApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMovieSwiftUIPADCApp.swift
+//  TheMovieSwiftUIPADC
+//
+//  Created by Wai Yan Moe on 18/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct TheMovieSwiftUIPADCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
