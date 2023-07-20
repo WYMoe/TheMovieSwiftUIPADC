a7322b0fc7423b19ac89c9eed36126d67fa48a94
Wai Yan Moe <waiyan.mdycode@gmail.com>
2023-07-20 14:43:26 +0630

    initial setup for realmDB

--- Pods/Realm/include/RLMPlatform.h
+++ Pods/Realm/include/RLMPlatform.h
@@ -1,3 +1,1 @@
-#define REALM_IOPLATFORMUUID @"WARNING: The active Xcode command line tools, as returned by 'xcode-select -p', are not from Xcode.
-         The newest version of Xcode will be used instead.
-ED931526-5DC2-5451-8AAB-FC5AD3253CFD"
+#define REALM_IOPLATFORMUUID @"WARNING: The active Xcode command line tools, as returned by 'xcode-select -p', are not from Xcode.The newest version of Xcode will be used instead.ED931526-5DC2-5451-8AAB-FC5AD3253CFD"
