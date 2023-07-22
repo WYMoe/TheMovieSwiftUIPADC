cc467c9853c2cdb0309b6b698473664223483fd0
Wai Yan Moe <waiyan.mdycode@gmail.com>
2023-07-22 23:51:36 +0630

    add genre obj

---
+++ TheMovieSwiftUIPADC/persistance/GenreObject.swift
@@ -0,0 +1,39 @@
+//
+//  GenreObject.swift
+//  TheMovieSwiftUIPADC
+//
+//  Created by Wai Yan Moe on 22/07/2023.
+//
+
+import Foundation
+import RealmSwift
+
+@objcMembers
+
+class GenreObject : Object,Codable {
+    @Persisted dynamic var id : Int?
+    @Persisted dynamic var name : String?
+    
+    var isSelected : Bool = false
+    
+    
+    override init() {
+        super.init()
+    }
+    
+    
+    init(id: Int? = nil, name: String? = nil, isSelected: Bool = false) {
+        super.init()
+        self.id = id
+        self.name = name
+        self.isSelected = isSelected
+    }
+    enum CodingKeys : String, CodingKey {
+        case id
+        case name
+    }
+    
+
+    
+
+}
