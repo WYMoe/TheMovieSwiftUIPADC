a4520b195391a934645127bb6598cd9b30682447
Wai Yan Moe <waiyan.mdycode@gmail.com>
2023-09-06 19:47:25 +0630

    MVVM contentviewmodel setup start

---
+++ TheMovieSwiftUIPADC/viewmodels/ContentViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ContentViewModel.swift
+//  TheMovieSwiftUIPADC
+//
+//  Created by Wai Yan Moe on 06/09/2023.
+//
+
+import Foundation
+
+class ContentViewModel : ObservableObject {
+    
+    
+    
+}
